969f18306df22dd4cdb80273f12946bd0367108b
Ahmed Zubayer Rahman <zubayer5566@gmail.com>
2026-01-16 15:25:53 +0600

    log2

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { GitHubBanner, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import Dashboard from "./pages/dashboard";
+import { Home } from "lucide-react"
 
 import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
+import { Layout } from "./components/refine-ui/layout/layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -29,14 +31,22 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "Q8ivzj-f3BkSp-9Ud25O",
               }}
+              resources={[
+                {
+                  name: 'dashboard', list: '/', meta:{label:'Home', icon: <Home />}
+                },
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }>
+                  
+                  <Route index element={<Dashboard />} />
+                </Route>
               </Routes>
-              <Toaster />
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
             </Refine>
             <DevtoolsPanel />
           </DevtoolsProvider>
